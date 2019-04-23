d9ff853547ff80256bc7f21fda111c13c695b5a9
Sam Foucart <samfoucart25@gmail.com>
2019-04-23 11:08:59 -0400

    Added App

---
+++ app/src/main/java/android/example/cs3560finalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package android.example.cs3560finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
